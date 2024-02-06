10044138fb1e3a30738efbb83cf106e1497b3332
SageMar <115049261+SageMar@users.noreply.github.com>
2024-02-05 18:22:18 -0800

    Update sign-up-form.php

--- sprint2/form-responses/sign-up-form.php
+++ sprint2/form-responses/sign-up-form.php
@@ -90,8 +90,6 @@ if (isset($_POST['email']) && $_POST['email'] != '' &&
     ?>
 
 <!--Scripts-->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.7/dist/umd/popper.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
