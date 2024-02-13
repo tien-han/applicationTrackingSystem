27464fd0eefc3426e1f36b2db9890adc83fbe0d9
Gene7Him <155666233+Gene7Him@users.noreply.github.com>
2024-02-13 11:41:20 -0800

    updated getOrder.php file to show orders that we have

--- pizza-order-form/data-processing/getOrder.php
+++ pizza-order-form/data-processing/getOrder.php
@@ -1,18 +1,18 @@
 <?php
-require '/home/cicadagr/db.php';
+require '/home/cicadagr/pizzadb.php';
 $sql = "SELECT * FROM Orders";
 $result = @mysqli_query($cnxn, $sql);
 while ($row = mysqli_fetch_assoc($result))
 {
     $orderID = $row['orderID'];
-    $customerID = $row['customerID'];
-    $priceID = $row['priceID'];
-    $promoID = $row['promoID'];
+    $Customers = $row['Customers'];
+    $Prices = $row['Prices'];
+    $promo = $row['promo'];
     $topping1 = $row['topping1'];
     $topping2 = $row['topping2'];
     $topping3 = $row['topping3'];
     $order_placed = $row['order_placed'];
     $delivery = $row['delivery'];
-    echo "<p>$orderID - $customerID, $priceID, $promoID, $topping1, $topping2, $topping3, $delivery, $order_placed</p>";
+    echo "<p>$orderID - $customerID, $priceID, $promo, $topping1, $topping2, $topping3, $delivery, $order_placed</p>";
 }
 ?>
