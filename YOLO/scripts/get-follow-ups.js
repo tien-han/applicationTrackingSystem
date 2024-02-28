322be8b8c2bc09ffe98d47f025a66626c79705d1
SageMar <115049261+SageMar@users.noreply.github.com>
2024-02-28 13:45:34 -0800

    Follow-up dates on student dashboard are loading AND are being filtered based on if they are 5 days + or - today

--- YOLO/scripts/get-follow-ups.js
+++ YOLO/scripts/get-follow-ups.js
@@ -3,6 +3,14 @@ window.addEventListener("load", function (event) {
 })
 
 async function getFollowUps() {
+    // first grab the date range +-5
+    let today = new Date();
+    let fiveDaysAhead = new Date(today);
+    fiveDaysAhead.setDate(today.getDate() + 5);
+    let fiveDaysLate = new Date(today);
+    fiveDaysLate.setDate(today.getDate() - 5)
+
+    // grab our data and create our rows
     await fetch("/YOLO/data-processing/get-recent-applications.php")
         .then(response => {
             if (!response.ok) {
@@ -17,26 +25,30 @@ async function getFollowUps() {
                 return;
             }
             data.forEach(application => {
-                const row = document.createElement('p');
-
-                <!-- we'll change the format of the dates so it doesn't display the h/m/s-->
                 let follow_up_date = new Date(application.follow_up_date);
-                let formattedDate = follow_up_date.toISOString().split('T')[0];
+                // compare the incoming dates to today to get ones within our range
+                if (fiveDaysLate <= follow_up_date && follow_up_date <= fiveDaysAhead) {
+                    const row = document.createElement('p');
 
-                row.innerHTML = `
-                   
-                        <!-- The following form method assigns the Id and allows us to direct to
-                        the correct form for updating/viewing so the user can follow up or say they did -->
-                        <form method="POST" action="../form-responses/edit-app-form.php">
-                            <input type = "hidden" name = "applicationId" value = "${application.applicationsId}">
-                            <p>Follow up with ${application.employer_name} by ${formattedDate}</p>
-                            <button type = "submit" class = "btn">View</button>
-                        </form>
-                   `;
-                reminders.appendChild(row);
+                    // we'll change the format of the dates so it doesn't display the h/m/s
+                    let follow_up_date = new Date(application.follow_up_date);
+                    let formattedDate = follow_up_date.toISOString().split('T')[0];
+
+                    row.innerHTML = `
+                       
+                            <!-- The following form method assigns the Id and allows us to direct to
+                            the correct form for updating/viewing so the user can follow up or say they did -->
+                            <form method="POST" action="../form-responses/edit-app-form.php">
+                                <input type = "hidden" name = "applicationId" value = "${application.applicationsId}">
+                                <p>Follow up with ${application.employer_name} by ${formattedDate}</p>
+                                <button type = "submit" class = "btn">View</button>
+                            </form>
+                       `;
+                    reminders.appendChild(row);
+                }
             });
         })
         .catch((error) => {
-            console.error('Error loading recent applications:', error);
+            console.error('Error loading follow ups:', error);
         });
 }
