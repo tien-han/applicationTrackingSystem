5dc74a0aaa452b4da89e2e62297a5858cdff00c3
SageMar <115049261+SageMar@users.noreply.github.com>
2024-03-06 13:42:36 -0800

    Added navbar to login page (with only the place order option) Added navbar to vieworders fixed redirect from direct connecting to vieworders (now takes to log-in if not logged in) Fixed pizza link for team page

--- pizza-order-form/data-processing/vieworders.php
+++ pizza-order-form/data-processing/vieworders.php
@@ -1,16 +1,15 @@
 <?php
-include ('pizza-order-form/index.php');
 // Start the session
 session_start();
 
+include ('pizza-order-form/index.php');
+
 // Get DB Connection credentials
 require '/home/cicadagr/pizzadb.php';
-
-
 // Check if the user is logged in
 if (!isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"]) {
     // Redirect to login.php if not logged in
-    header("Location: index.php");
+    header("Location: ../index.php");
     exit();
 }
 
@@ -20,6 +19,37 @@ if (!isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"]) {
 $ordersSQL = "SELECT * FROM Orders";
 $result = mysqli_query($cnxn, $ordersSQL);
 
+// add the header
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>View Orders</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="../css/styles.css">
+</head>
+<body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand navbar-dark" role="navigation">
+    <!-- Navbar Brand & Toggler -->
+    <div class="navbar-header" id="navbar-header">
+        <a class="navbar-brand navbar-left px-3"><img
+                    alt="Pizza Logo from Pexels.com" src="/pictures/pexels-christopher-farrugia-3581878.jpg" width="100"></a>
+    </div>
+    <div class="navbar-expand-sm me-auto">
+        <ul class="navbar-nav align-items-center">
+            <li class="nav-item">
+                <a class="nav-link" href="../placeOrder/placeOrder.html">Place Your Order</a>
+                <a class="nav-link active" href="viewOrders.php">All Orders</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<?php
 // Check if there are any orders
 if (mysqli_num_rows($result) > 0) {
     echo "<h2>All Orders</h2>";
@@ -59,4 +89,8 @@ if (mysqli_num_rows($result) > 0) {
 // Close the database connection
 mysqli_close($cnxn);
 ?>
+
+<!-- close html +body-->
+</body>
+</html>
 
--- pizza-order-form/index.php
+++ pizza-order-form/index.php
@@ -22,11 +22,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
     <title>Login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
+
 <body>
+<!-- Navbar -->
+<nav class="navbar navbar-expand navbar-dark" role="navigation">
+    <!-- Navbar Brand & Toggler -->
+    <div class="navbar-header" id="navbar-header">
+        <a class="navbar-brand navbar-left px-3"><img
+                    alt="Pizza Logo from Pexels.com" src="/pictures/pexels-christopher-farrugia-3581878.jpg" width="100"></a>
+    </div>
+    <div class="navbar-expand-sm me-auto">
+        <ul class="navbar-nav align-items-center">
+            <li class="nav-item">
+                <a class="nav-link active" href="placeOrder/placeOrder.html">Place Your Order</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 <h2>Login</h2>
 <?php
 if (isset($error_message)) {
