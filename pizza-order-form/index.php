cad80fd34d3c8f8c580ecb59fcaf8c28c4f175e0
SageMar <115049261+SageMar@users.noreply.github.com>
2024-03-07 14:18:14 -0800

    Fixed links

--- pizza-order-form/index.php
+++ pizza-order-form/index.php
@@ -27,7 +27,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="/pizza-order-form/css/styles.css">
 </head>
 
 <body>
