2256671cd7130121eacb3dcb070971ca4cd0fbd0
cmatthews444 <cmatthews4278@gmail.com>
2024-03-17 22:57:28 -0700

    Logout, fixed character limit closeable followups and follow up limit

--- blobbieblob/data-processing/get-recent-applications.php
+++ blobbieblob/data-processing/get-recent-applications.php
@@ -7,6 +7,9 @@
     $recentApplications = [];
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_assoc($result)) {
+            if (isset($row['role_name'])) {
+                $row['role_name'] = substr($row['role_name'], 0, 40); //limits the characters for role_name
+            }
             $recentApplications[] = $row;
         }
     } else {
